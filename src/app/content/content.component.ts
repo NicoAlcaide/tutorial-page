f75e986576f7aa314a35462bcaea221033c5bf6f
NicoAlcaide <nicolas.alcaide714@comunidadunir.net>
2024-12-15 16:31:22 +0100

    Update content.component.ts

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -4,9 +4,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
+  styleUrl: './content.component.css',
   imports: [RouterOutlet] // Importar RouterOutlet aquí
 })
 export class ContentComponent {}
