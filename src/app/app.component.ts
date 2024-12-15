7a7c16029f179e968f5820624ec6a2acefd84738
NicoAlcaide <nicolas.alcaide714@comunidadunir.net>
2024-12-15 16:31:31 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, SidebarComponent, ContentComponent, FooterComponent] // Asegúrate de importar los componentes aquí
+  imports: [HeaderComponent, ContentComponent, FooterComponent] // Asegúrate de importar los componentes aquí
 })
 export class AppComponent {
   title = 'tutorial-page';
